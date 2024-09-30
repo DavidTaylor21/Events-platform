ab03226db109e298982c0f34c987a1a43db4180e
David Taylor <davidtaylor@Davids-MacBook-Pro.local>
2024-09-30 11:32:00 +0100

    App setup

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at PORT: ${PORT}`);
+})
